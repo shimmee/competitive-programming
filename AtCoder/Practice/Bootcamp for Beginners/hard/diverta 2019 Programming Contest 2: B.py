23f363bc77c52b051a17dba0e19bc19237f9fad7
Keita Shimmei <shinmeikeita@gmail.com>
2021-01-21 10:16:22 -0500

    no comments

---
+++ AtCoder/Practice/Bootcamp for Beginners/hard/diverta 2019 Programming Contest 2: B.py
@@ -0,0 +1,29 @@
+# diverta 2019 Programming Contest 2: B - Picking Up
+# Bootcamp For Beginners - Hard
+# URL: https://atcoder.jp/contests/diverta2019-2/tasks/diverta2019_2_b
+# Date: 2021/01/20
+
+# ---------- Ideas ----------
+#
+
+# ------------------- Solution --------------------
+#
+
+# ------------------- Answer --------------------
+#code:python
+
+
+# ------------------ Sample Input -------------------
+
+
+# ----------------- Length of time ------------------
+#
+
+# -------------- Editorial / my impression -------------
+#
+
+# ----------------- Category ------------------
+#AtCoder
+#BootcampForBeginners-hard
+#AC_with_editorial #解説AC
+#wanna_review #hard復習 #復習したい
